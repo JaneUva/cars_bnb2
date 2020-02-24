5d1e24d1d7bcfda9892a11b6f4beca2e62a90bb3
Danny Iacono <danny.iacono@contentful.com>
2020-02-24 18:04:08 +0100

    Validations for each model added.

--- app/models/car.rb
+++ app/models/car.rb
@@ -1,2 +1,5 @@
 class Car < ApplicationRecord
+  has_many :bookings, dependent: :destroy
+  has_many :users, through: :bookings
+  validates :capacity, :brand, :type, :price, :drive_train presence: true
 end
