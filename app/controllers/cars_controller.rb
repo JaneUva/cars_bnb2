e044da04ce3cb4b6e0e3e936f7388e878bcbcfa4
Mariken Gulpen <marikenvangulpen@gmail.com>
2020-02-25 13:57:40 +0100

    added extra end in CarsController

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,13 +1,15 @@
 class CarsController < ApplicationController
 skip_before_action :authenticate_user!, only: [:index, :show]
+
   def index
      if params[:query].present?
        @cars = Car.where("brand ILIKE?", "%#{params[:query]}%")
      else
        @cars = policy_scope(Car)
      end
+  end
 
-    def show
+  def show
     @car = Car.find(params[:id])
     authorize @car
   end
