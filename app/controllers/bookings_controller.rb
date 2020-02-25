2d82809bd874477326906d825d887b83ca4a8ddc
Danny Iacono <danny.iacono@contentful.com>
2020-02-25 13:45:03 +0100

    Got the button and form and loose style in place. Current form includes date picker. Need to sort out proper routing and display of bookings now.

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,10 +1,30 @@
 class BookingsController < ApplicationController
+  before_action :set_car, except: :destroy
   def create
+    @booking = Booking.new(set_params)
+    @booking.car = @car
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      render :show
+    end
   end
 
   def show
+    @booking = Booking.find(params[:id])
   end
 
   def destroy
   end
+
+  private
+
+  def set_car
+    @car = Car.find(params[:car_id])
+  end
+
+  def set_params
+    params[:booking].permit(:start_date, :end_date, :car_id, :user_id)
+  end
+
 end
--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -3,6 +3,7 @@ class CarsController < ApplicationController
   end
 
   def show
+    @booking = Booking.new
     @car = Car.find(params[:id])
   end
 end
