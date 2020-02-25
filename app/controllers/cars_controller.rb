3944a576337e4e9ee2447d85afa390c3f6b43c47
Mariken Gulpen <marikenvangulpen@gmail.com>
2020-02-25 12:12:49 +0100

    adding search bar to the navbar

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,8 +1,21 @@
 class CarsController < ApplicationController
-  def index
-  end
+
 
   def show
     @car = Car.find(params[:id])
   end
+
+  def self.search(search)
+    if search
+      car_kind = Car.find_by(kind: search)
+        if car_kind
+          self.where(car_id: car_kind)
+        else
+          @cars=Car.all
+        end
+    else
+      @cars = Car.all
+    end
+  end
+
 end
