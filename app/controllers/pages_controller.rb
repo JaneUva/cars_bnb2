4a5e22de13eef81ae42f9610d5da32a9a2618dcb
Jana Uvarova <uvarova.jana@gmail.com>
2020-02-25 11:22:26 +0100

    added def for home in pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: :home
   def home
+    @cars = Car.all
   end
 end
