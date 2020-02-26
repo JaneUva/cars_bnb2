c0a9a4bb19b39a55ef7e409ac56104e04a64fb96
Jana Uvarova <uvarova.jana@gmail.com>
2020-02-26 15:30:22 +0100

    fixed error rendering

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,13 +1,16 @@
 class ReviewsController < ApplicationController
   def create
+    @booking = Booking.new
     @car = Car.find(params[:car_id])
     @review = Review.new(review_params)
     @review.car = @car
     @review.user_id = current_user.id
     authorize @review
     if @review.save
+      # Happy path
       redirect_to car_path(@car)
     else
+      # Sad path
       render 'cars/show'
     end
   end
