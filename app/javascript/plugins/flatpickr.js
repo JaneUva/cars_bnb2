8c6d9468e6eb110bfaf11f6be48206c5b77c17eb
Markus Mengel <markus.mengelhx@gmail.com>
2020-02-26 17:07:53 +0100

    flatpickr is working yay

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,8 +1,37 @@
-import flatpickr from "flatpickr";
+import flatpickr from "flatpickr"
 import 'flatpickr/dist/themes/airbnb.css'
-// import "flatpickr/dist/flatpickr.min.css";
-import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
+//import "flatpickr/dist/flatpickr.min.css";
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin"
 
 flatpickr("#start_date", {
+  altInput: true,
   "plugins": [new rangePlugin({ input: "#end_date"})]
 });
+
+const startDateInput = document.getElementById('booking_start_date');
+const endDateInput = document.getElementById('booking_end_date');
+
+// Check that the query selector id matches the one you put around your form.
+if (startDateInput) {
+const unavailableDates = JSON.parse(document.querySelector('#booking_date_picker').dataset.unavailable)
+endDateInput.disabled = true
+
+flatpickr(startDateInput, {
+  minDate: "today",
+  disable: unavailableDates,
+  dateFormat: "Y-m-d",
+});
+
+console.log('im in the file')
+
+startDateInput.addEventListener("change", (e) => {
+  if (startDateInput != "") {
+    endDateInput.disabled = false
+  }
+  flatpickr(endDateInput, {
+    minDate: e.target.value,
+    disable: unavailableDates,
+    dateFormat: "Y-m-d"
+    });
+  })
+};
--- app/models/car.rb
+++ app/models/car.rb
@@ -2,4 +2,10 @@ class Car < ApplicationRecord
   has_many :bookings, dependent: :destroy
   has_many :users, through: :bookings
   validates :capacity, :brand, :kind, :price, :drive_train, presence: true
+
+  def unavailable_dates
+    bookings.pluck(:start_date, :end_date).map do |range|
+      { from: range[0], to: range[1] }
+    end
+  end
 end
