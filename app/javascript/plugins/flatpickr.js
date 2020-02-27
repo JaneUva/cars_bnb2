671ef1b14c2ff88948a2229a7e11333cd40928cf
Markus Mengel <markus.mengelhx@gmail.com>
2020-02-27 11:33:32 +0100

    removed unneeded css in flatpickr

--- app/javascript/plugins/flatpickr.js
+++ app/javascript/plugins/flatpickr.js
@@ -1,5 +1,4 @@
 import flatpickr from "flatpickr";
-// import 'flatpickr/dist/themes/airbnb.css';
 import "flatpickr/dist/flatpickr.min.css";
 import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
 
