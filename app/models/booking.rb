7fc6364dc089399feaf599b86b24295580be9ace
Jana Uvarova <uvarova.jana@gmail.com>
2020-02-28 17:08:38 +0100

    turn off faulty date validation

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,9 +2,9 @@ class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :car
   validates :start_date, :end_date, presence: true
-  validate :start_date_cannot_be_in_the_past
-  validate :end_date_must_be_after_start_date
-
+  # validate :start_date_cannot_be_in_the_past
+  # validate :end_date_must_be_after_start_date
+#
   def start_date_cannot_be_in_the_past
     errors.add(:start_date, "can't be in the past") if
       self.start_date <= Date.today
